34ad63b15fbf973d189192146fb700e51fe34b98
Erik Gassler <erikgassler@live.com>
2013-08-13 12:15:42 -0400

    0.6.4
    Added delay for startup to allow settings to get loaded before
    attempting to run setup. Added sublime context menu for user settings,
    meant to go in last update.

--- MySnippets.py
+++ MySnippets.py
@@ -4,6 +4,12 @@ import sublime_plugin
 import threading
 import re
 
+def setup():
+	# Load all of our package settings into a global object
+	settings = sublime.load_settings('MySnippets.sublime-settings')
+	settings.add_on_change('changed',buildsnippets)
+	buildsnippets()
+
 def buildfolder(path, nt):
 	strReturn = ''
 	strFolder = ''
@@ -31,41 +37,68 @@ def buildfolder(path, nt):
 	return strFolder + strReturn
 
 def buildsnippets():
+	# Root path for This Package
+	root = sublime.packages_path().replace('\\','/') + '/My Snippets/'
+	settings = sublime.load_settings('MySnippets.sublime-settings')
+
+	# Make sure user settings file exists
+	if os.path.exists(sublime.packages_path().replace('\\','/') + '/User/MySnippets.sublime-settings') == False:
+		print("Creating: " + sublime.packages_path().replace('\\','/') + '/User/MySnippets.sublime-settings')
+		subset = open(sublime.packages_path().replace('\\','/') + '/User/MySnippets.sublime-settings', 'w')
+		subset.write('{'\
+			+ '\n\t// Setup other folders that contain code snippets you want to include in your library'\
+			+ '\n\t// Each folder setting must have a "display" and "path" name|value pair'\
+			+ '\n\t// If path is empty, or if path is not an accessible directory, then that setting will be skipped'\
+			+ '\n\t// Leaving display empty will place directory contents in root menu'\
+			+ '\n\t"folders":['
+			+ '\n\t\t{'\
+			+ '\n\t\t\t"display": "",'\
+			+ '\n\t\t\t"path": ""'\
+			+ '\n\t\t}'\
+			+ '\n\t]'\
+			+ '\n}\n')
+		subset.close()
+
 	# Get folders as paths to sync snippets from
 	paths = settings.get("folders")
 
 	# Create the context submenu based on the current library of snippets
+	print(paths)
 
 	# build menus from settings
-	strPaths = ''
-	nt = '\n\t\t\t'
-	try:
-		for path in paths:
-			if "path" in path and path['path'] != '' and os.path.isdir(path['path']):
-				strTemp = buildfolder(path['path'],nt + '\t\t')
-				if strTemp != '':
-					if strPaths != '':
-						strPaths += ','
-					if 'display' in path and path['display'] != '':
-						strPaths += nt + '{'\
-								 + nt + '\t"caption": "' + path['display'] + '",'\
-								 + nt + '\t"children": ['
-						strPaths += strTemp
-						strPaths += nt + '\t]' + nt + '}'
-					else:
-						strPaths += strTemp
-	except:
-		print('Invalid path')
+	if paths != None:
+		strPaths = ''
+		nt = '\n\t\t\t'
+		try:
+			for path in paths:
+				if "path" in path and path['path'] != '' and os.path.isdir(path['path']):
+					strTemp = buildfolder(path['path'],nt + '\t\t')
+					if strTemp != '':
+						if strPaths != '':
+							strPaths += ','
+						if 'display' in path and path['display'] != '':
+							strPaths += nt + '{'\
+									 + nt + '\t"caption": "' + path['display'] + '",'\
+									 + nt + '\t"children": ['
+							strPaths += strTemp
+							strPaths += nt + '\t]' + nt + '}'
+						else:
+							strPaths += strTemp
+		except:
+			print('Invalid path')
 
-	# build file for context menu
-	if strPaths != '':
-		submen = open(root + "Context.sublime-menu", 'w')
-		submen.write('[\n\t{\n\t\t"id":"my-snippets",\n\t\t"caption":"My Snippets",\n\t\t"children":[')
-		submen.write(strPaths)
-		submen.write("\n\t\t]\n\t}\n]\n")
-		submen.close()
+		# build file for context menu
+		if strPaths != '':
+			submen = open(root + "Context.sublime-menu", 'w')
+			submen.write('[\n\t{\n\t\t"id":"my-snippets",\n\t\t"caption":"My Snippets",\n\t\t"children":[')
+			submen.write(strPaths)
+			submen.write("\n\t\t]\n\t}\n]\n")
+			submen.close()
+			print('Snippets Menu Built.')
+		else:
+			print('No snippets found: Context Menu not created.')
 	else:
-		print('No snippets found: Context Menu not created.')
+		print('No "folders" found in settings.')
 
 # This command gets run from the context menu selections
 class mysnippetsCommand(sublime_plugin.TextCommand):
@@ -100,29 +133,4 @@ class snippetbuilder(threading.Thread):
 	def run(self):
 		buildsnippets()
 
-# Root path for This Package
-root = sublime.packages_path().replace('\\','/') + '/My Snippets/'
-
-# Make sure user settings file exists
-if not os.path.exists(sublime.packages_path().replace('\\','/') + '/User/MySnippets.sublime-settings'):
-	subset = open(sublime.packages_path().replace('\\','/') + '/User/MySnippets.sublime-settings', 'w')
-	subset.write('{'\
-		+ '\n\t// Setup other folders that contain code snippets you want to include in your library'\
-		+ '\n\t// Each folder setting must have a "display" and "path" name|value pair'\
-		+ '\n\t// If path is empty, or if path is not an accessible directory, then that setting will be skipped'\
-		+ '\n\t// Leaving display empty will place directory contents in root menu'\
-		+ '\n\t"folders":['
-		+ '\n\t\t{'\
-		+ '\n\t\t\t"display": "",'\
-		+ '\n\t\t\t"path": ""'\
-		+ '\n\t\t}'\
-		+ '\n\t]'\
-		+ '\n}\n')
-	subset.close()
-
-# Load all of our package settings into an object
-settings = sublime.load_settings('MySnippets.sublime-settings')
-
-settings.add_on_change('changed',buildsnippets)
-
-buildsnippets()
+sublime.set_timeout(lambda: setup(), 2000)
